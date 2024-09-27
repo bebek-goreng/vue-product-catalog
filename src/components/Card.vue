d48be493392f0725fb1f661de41cea85205fb26a
mahen <oresuki01@gmail.com>
2024-09-27 19:08:27 +0700

    add: card component

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+}
+</script>
+
+<style scoped src="@/assets/styles/Card.css"></style>
