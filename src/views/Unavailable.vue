ea8e83faf01c5045509cb72dc39ebb23d0e9c29a
mahen <oresuki01@gmail.com>
2024-09-29 18:51:19 +0700

    add: unavailable product component

---
+++ src/views/Unavailable.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="unavailable-product-container">
+        <p>This product is unavailable to show</p>
+        <button @click="handleClick">{{ buttonName }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UnavailableProduct",
+    props: {
+        handleClick: {
+            type: Function,
+            required: true,
+        },
+        buttonName: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped src="../assets/styles/UnavailableProduct.css"></style>
